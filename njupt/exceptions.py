e9646707f81d0dd42efe160fd6d27593fd2d0215
gaoliang <gaoliangim@gmail.com>
2018-04-07 18:07:39 +0800

    add a coursers interface

--- njupt/exceptions.py
+++ njupt/exceptions.py
@@ -2,18 +2,22 @@
 __author__ = 'gaoliang'
 
 
-class NjuptError(Exception):
+class NjuptException(Exception):
     """Base class for exceptions in this module."""
 
     def __init__(self, *args, **kwargs):
-        super(NjuptError, self).__init__(*args, **kwargs)
+        super(NjuptException, self).__init__(*args, **kwargs)
 
 
-class ZhengfangNotLogin(NjuptError):
+class AuthenticationException(NjuptException):
+    """An Authentication Exception occurred."""
+
+
+class ZhengfangNotLogin(NjuptException):
     def __init__(self, message="正方账户未登录"):
         super(ZhengfangNotLogin, self).__init__(message)
 
 
-class CardNotLogin(NjuptError):
+class CardNotLogin(NjuptException):
     def __init__(self, message="一卡通账号未登录"):
         super(CardNotLogin, self).__init__(message)
